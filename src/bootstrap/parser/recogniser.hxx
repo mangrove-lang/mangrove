e01fb5a7cf0d59e532b2f7e5b570101cdc0c076e
dragonmux <git@dragonmux.network>
2022-12-06 10:14:37 +0000

    bootstrap/parser: Implemented stubs for the various major components needed for the tokeniser

---
+++ src/bootstrap/parser/recogniser.hxx
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#ifndef PARSER_RECOGNISER_HXX
+#define PARSER_RECOGNISER_HXX
+
+namespace mangrove::parser::recognisers
+{
+} // namespace mangrove::parser::recognisers
+
+#endif /*PARSER_RECOGNISER_HXX*/
---
+++ src/bootstrap/parser/tokeniser.hxx
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#ifndef PARSER_TOKENISER_HXX
+#define PARSER_TOKENISER_HXX
+
+#include "recogniser.hxx"
+#include "types.hxx"
+
+namespace mangrove::parser
+{
+} // namespace mangrove::parser
+
+#endif /*PARSER_TOKENISER_HXX*/
