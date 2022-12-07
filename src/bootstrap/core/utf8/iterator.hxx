0fc6caf18bb0eca84a5e83c77b7ed653b051ee95
dragonmux <git@dragonmux.network>
2022-12-07 11:57:33 +0000

    bootstrap/core/utf8: Split the StringIterator out into its own header

---
+++ src/bootstrap/core/utf8/iterator.hxx
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#ifndef CORE_UTF8_ITERATOR_HXX
+#define CORE_UTF8_ITERATOR_HXX
+
+#include "char.hxx"
+
+namespace mangrove::core::utf8::iterator
+{
+	struct StartAtEnd final  { };
+
+	// Please note, this is ONLY valid on strings that are entirely valid UTF-8.
+	struct StringIterator final
+	{
+	private:
+		const char *_data;
+		// length and offset are in bytes for simplicity..
+		size_t _length;
+		size_t _offset{};
+
+	public:
+		constexpr StringIterator(const char *data, size_t length) : _data{data}, _length{length} { }
+		constexpr StringIterator(const std::string_view &str) : _data{str.data()}, _length{str.length()} { }
+		constexpr StringIterator(const char *data, size_t length, StartAtEnd) :
+			_data{data}, _length{length}, _offset{_length} { }
+		constexpr StringIterator(const std::string_view &str, StartAtEnd) :
+			_data{str.data()}, _length{str.length()}, _offset{_length} { }
+
+		constexpr Char operator *() const noexcept
+			{ return {std::string_view{_data + _offset, _length}}; }
+
+		constexpr StringIterator &operator ++() noexcept
+		{
+			if (_offset == _length)
+				return *this;
+			const auto chr{**this};
+			_offset += chr.length();
+			return *this;
+		}
+
+		constexpr StringIterator &operator --() noexcept
+		{
+			if (!_offset)
+				return *this;
+			auto offset{_offset - 1U};
+			while (offset > 0 && (_data[offset] & 0xc0U) == 0x80U)
+				--offset;
+			_offset = offset;
+			return *this;
+		}
+
+		[[nodiscard]] constexpr inline bool operator ==(const StringIterator &other) noexcept
+			{  return _data == other._data && _length == other._length && _offset == other._offset; }
+		[[nodiscard]] constexpr inline bool operator !=(const StringIterator &other) noexcept
+			{  return _data != other._data || _length != other._length || _offset != other._offset; }
+
+		using difference_type = ptrdiff_t;
+		using value_type = Char;
+		using pointer = void;
+		using reference = Char;
+		using iterator_category = std::bidirectional_iterator_tag;
+	};
+} // namespace mangrove::core::utf8::iterator
+
+#endif /*CORE_UTF8_ITERATOR_HXX*/
--- src/bootstrap/core/utf8/string.hxx
+++ src/bootstrap/core/utf8/string.hxx
@@ -3,6 +3,7 @@
 #define CORE_UTF8_STRING_HXX
 
 #include "char.hxx"
+#include "iterator.hxx"
 
 namespace mangrove::core::utf8
 {
@@ -59,60 +60,6 @@ namespace mangrove::core::utf8
 			}
 			return count;
 		}
-
-		struct StartAtEnd final  { };
-
-		// Please note, this is ONLY valid on strings that are entirely valid UTF-8.
-		struct StringIterator final
-		{
-		private:
-			const char *_data;
-			// length and offset are in bytes for simplicity..
-			size_t _length;
-			size_t _offset{};
-
-		public:
-			constexpr StringIterator(const char *data, size_t length) : _data{data}, _length{length} { }
-			constexpr StringIterator(const std::string_view &str) : _data{str.data()}, _length{str.length()} { }
-			constexpr StringIterator(const char *data, size_t length, StartAtEnd) :
-				_data{data}, _length{length}, _offset{_length} { }
-			constexpr StringIterator(const std::string_view &str, StartAtEnd) :
-				_data{str.data()}, _length{str.length()}, _offset{_length} { }
-
-			constexpr Char operator *() const noexcept
-				{ return {std::string_view{_data + _offset, _length}}; }
-
-			constexpr StringIterator &operator ++() noexcept
-			{
-				if (_offset == _length)
-					return *this;
-				const auto chr{**this};
-				_offset += chr.length();
-				return *this;
-			}
-
-			constexpr StringIterator &operator --() noexcept
-			{
-				if (!_offset)
-					return *this;
-				auto offset{_offset - 1U};
-				while (offset > 0 && (_data[offset] & 0xc0U) == 0x80U)
-					--offset;
-				_offset = offset;
-				return *this;
-			}
-
-			[[nodiscard]] constexpr inline bool operator ==(const StringIterator &other) noexcept
-				{  return _data == other._data && _length == other._length && _offset == other._offset; }
-			[[nodiscard]] constexpr inline bool operator !=(const StringIterator &other) noexcept
-				{  return _data != other._data || _length != other._length || _offset != other._offset; }
-
-			using difference_type = ptrdiff_t;
-			using value_type = Char;
-			using pointer = void;
-			using reference = Char;
-			using iterator_category = std::bidirectional_iterator_tag;
-		};
 	} // namespace internal
 
 	struct String
@@ -157,10 +104,10 @@ namespace mangrove::core::utf8
 		auto size() const noexcept { return _length; }
 		auto byteLength() const noexcept { return _data.size(); }
 
-		auto begin() noexcept { return StringIterator{_data}; }
-		const auto begin() const noexcept { return StringIterator{_data}; }
-		auto end() noexcept { return StringIterator{_data, StartAtEnd{}}; }
-		const auto end() const noexcept { return StringIterator{_data, StartAtEnd{}}; }
+		auto begin() noexcept { return iterator::StringIterator{_data}; }
+		const auto begin() const noexcept { return iterator::StringIterator{_data}; }
+		auto end() noexcept { return iterator::StringIterator{_data, iterator::StartAtEnd{}}; }
+		const auto end() const noexcept { return iterator::StringIterator{_data, iterator::StartAtEnd{}}; }
 
 		auto rbegin() noexcept { return std::reverse_iterator{end()}; }
 		const auto rbegin() const noexcept { return std::reverse_iterator{end()}; }
