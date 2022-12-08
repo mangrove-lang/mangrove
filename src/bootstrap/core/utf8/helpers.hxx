8d421c6597dc87dbaa97c1c683fb567ab376d1b6
dragonmux <git@dragonmux.network>
2022-12-08 00:59:03 +0000

    bootstrap/core/utf8: Split out the code point helpers into their own header for logistical reasons

---
+++ src/bootstrap/core/utf8/helpers.hxx
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: BSD-3-Clause
+#ifndef CORE_UTF8_HELPERS_HXX
+#define CORE_UTF8_HELPERS_HXX
+
+#include <cstdint>
+#include <cstddef>
+#include <string_view>
+
+namespace mangrove::core::utf8::helpers
+{
+	constexpr static inline uint8_t safeIndex(std::string_view str, const size_t index) noexcept
+	{
+		if (index > str.size())
+			return UINT8_MAX;
+		return uint8_t(str[index]);
+	}
+
+	constexpr inline bool isMultiValid() noexcept { return true; }
+	template<typename... values_t> constexpr inline bool isMultiValid(const uint8_t c, values_t ...values) noexcept
+		{ return (c & 0xc0U) == 0x80U && isMultiValid(values...); }
+
+	constexpr static inline size_t countUnits(const std::string_view &str) noexcept
+	{
+		size_t count{};
+		for (size_t i = 0; i < str.size(); ++i)
+		{
+			const auto byteA{safeIndex(str, i)};
+			// Multiple code unit encoded character?
+			if (byteA & 0x80U)
+			{
+				const auto byteB{safeIndex(str, ++i)};
+				if ((byteA & 0x60U) == 0x40U)
+				{
+					// 2 code units.. check that the second unit is valid and return 0 if not.
+					if (!isMultiValid(byteB))
+						return 0;
+				}
+				else if ((byteA & 0x70U) == 0x60U)
+				{
+					// 3 code units.. check that the second and third units are valid and return 0 if not
+					if (!isMultiValid(byteB, safeIndex(str, ++i)))
+						return 0;
+					// Also check that the code point is valid (not D800-DFFF)
+					else if ((byteA & 0x0F) == 0x0D && (byteB & 0x20))
+						return 0;
+				}
+				else if ((byteA & 0x78U) == 0x70U)
+				{
+					// 4 code units.. check that the second, third and fourth unit is valid
+					if (!isMultiValid(byteB, safeIndex(str, i + 1), safeIndex(str, i + 2)))
+						return 0;
+					i += 2;
+					++count;
+				}
+				else
+					return 0;
+			}
+			++count;
+		}
+		return count;
+	}
+} // namespace mangrove::core::utf8::helpers
+
+#endif /*CORE_UTF8_HELPERS_HXX*/
