29851288b27c348bd08cfcbdc2c7ffa907d8eebd
dragonmux <git@dragonmux.network>
2022-12-08 00:59:54 +0000

    bootstrap/core/utf8: Implemented iteration and accessors for StringView

--- src/bootstrap/core/utf8/stringView.hxx
+++ src/bootstrap/core/utf8/stringView.hxx
@@ -4,6 +4,7 @@
 
 #include <cstddef>
 #include <string_view>
+#include "helpers.hxx"
 
 namespace mangrove::core::utf8
 {
@@ -14,7 +15,25 @@ namespace mangrove::core::utf8
 		size_t _length;
 
 	public:
-		constexpr StringView(std::string_view data, size_t codeUnits) noexcept : _data{data}, _length{codeUnits} { }
+		constexpr StringView(const std::string_view &data) noexcept : _data{data}, _length{helpers::countUnits(_data)} { }
+		constexpr StringView(const std::string_view &data, const size_t codeUnits) noexcept :
+			_data{data}, _length{codeUnits} { }
+
+		auto data() noexcept { return _data.data(); }
+		const auto *data() const noexcept { return _data.data(); }
+		auto length() const noexcept { return _length; }
+		auto size() const noexcept { return _length; }
+		auto byteLength() const noexcept { return _data.size(); }
+
+		auto begin() noexcept { return iterator::StringIterator{_data}; }
+		const auto begin() const noexcept { return iterator::StringIterator{_data}; }
+		auto end() noexcept { return iterator::StringIterator{_data, iterator::StartAtEnd{}}; }
+		const auto end() const noexcept { return iterator::StringIterator{_data, iterator::StartAtEnd{}}; }
+
+		auto rbegin() noexcept { return std::reverse_iterator{end()}; }
+		const auto rbegin() const noexcept { return std::reverse_iterator{end()}; }
+		auto rend() noexcept { return std::reverse_iterator{begin()}; }
+		const auto rend() const noexcept { return std::reverse_iterator{begin()}; }
 	};
 } // namespace mangrove::core::utf8
 
