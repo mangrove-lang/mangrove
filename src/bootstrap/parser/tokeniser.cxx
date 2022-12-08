33b76eef817fe8239df6fd6f5cc90686ac0650d9
dragonmux <git@dragonmux.network>
2022-12-08 04:48:50 +0000

    bootstrap/parser: Begun implementing the tokeniser

--- src/bootstrap/parser/tokeniser.cxx
+++ src/bootstrap/parser/tokeniser.cxx
@@ -1,2 +1,21 @@
 // SPDX-License-Identifier: BSD-3-Clause
 #include "tokeniser.hxx"
+
+using namespace mangrove::parser;
+using namespace mangrove::parser::types;
+
+Token &Tokeniser::next() noexcept
+{
+	if (_file.isEOF())
+	{
+		_token.set(TokenType::eof);
+		return _token;
+	}
+	_token.reset();
+	readToken();
+	return _token;
+}
+
+void Tokeniser::readToken() noexcept
+{
+}
--- src/bootstrap/parser/tokeniser.hxx
+++ src/bootstrap/parser/tokeniser.hxx
@@ -2,11 +2,35 @@
 #ifndef PARSER_TOKENISER_HXX
 #define PARSER_TOKENISER_HXX
 
+#include <utility>
+#include <substrate/fd>
 #include "recogniser.hxx"
 #include "types.hxx"
 
 namespace mangrove::parser
 {
+	inline namespace internal
+	{
+		using substrate::fd_t;
+	} // namespace internal
+
+	struct Tokeniser final
+	{
+	private:
+		fd_t _file;
+		Char currentChar{};
+		types::Position position{};
+		types::Token _token{};
+
+		void readToken() noexcept;
+
+	public:
+		Tokeniser(fd_t &&file) : _file{std::move(file)} { }
+
+		auto token() const noexcept { return _token; }
+
+		types::Token &next() noexcept;
+	};
 } // namespace mangrove::parser
 
 #endif /*PARSER_TOKENISER_HXX*/
