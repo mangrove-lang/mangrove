b4ff46732b443e1bef619374b5e017601187cb71
dragonmux <git@dragonmux.network>
2022-12-08 02:28:50 +0000

    bootstrap/core/utf8: Copied .substr(), .beginsWith() and .sendsWith() from String onto StringView

--- src/bootstrap/core/utf8/stringView.hxx
+++ src/bootstrap/core/utf8/stringView.hxx
@@ -34,6 +34,65 @@ namespace mangrove::core::utf8
 		constexpr auto rbegin() const noexcept { return std::reverse_iterator{end()}; }
 		constexpr auto rend() noexcept { return std::reverse_iterator{begin()}; }
 		constexpr auto rend() const noexcept { return std::reverse_iterator{begin()}; }
+
+		StringView substr(size_t offset, size_t count = npos) const noexcept
+		{
+			size_t begin{};
+			// If the starting point is less than the number of characters in the string
+			if (offset < length())
+			{
+				// Figure out the offset for the first character requested
+				for (size_t i = 0; begin < byteLength() && i < offset; ++i)
+					begin += Char{_data.substr(begin)}.length();
+			}
+			else
+			{
+				// Otherwise it's just the end of this string
+				offset = length();
+				begin = byteLength();
+			}
+
+			// Bring count into range
+			if (count > length() - offset)
+				count = length() - offset;
+
+			auto end{begin};
+			// Now we have [begin, ) but need to figure out the end offset - so go forward count characters
+			for (size_t i = 0; i < count; ++i)
+				end += Char{_data.substr(end)}.length();
+
+			// Finally, we have the substring range and the number of UTF-8 characters in it, make the substring
+			return {_data.substr(begin, end), count};
+		}
+
+		bool beginsWith(const StringView &str) const noexcept
+		{
+			// Start by making sure the string to compare is not longer than this one
+			if (str.length() > length() || str.byteLength() > byteLength())
+				return false;
+			// Then extract the raw data begin/end
+			const auto *const begin{str.data()};
+			const auto *const end{begin + str.byteLength()};
+			// And bytewise compare for equality
+			return std::equal(begin, end, _data.cbegin());
+		}
+
+		bool endsWith(const StringView &str) const noexcept
+		{
+			// Start by making sure the string to compare is not longer than this one
+			if (str.length() > length() || str.byteLength() > byteLength())
+				return false;
+			// Then extract the raw data begin/end
+			const auto *const begin{str.data()};
+			const auto *const end{begin + str.byteLength()};
+			// And bytewise compare for equality, rewinding from the end of this string
+			return std::equal(begin, end, _data.cend() - str.byteLength());
+		}
+
+		bool startsWith(const StringView &str) const noexcept
+			{ return beginsWith(str); }
+
+		constexpr static size_t npos{SIZE_MAX};
 	};
 } // namespace mangrove::core::utf8
 
