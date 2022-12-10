2c7595e1bb6ba93464fa2ac3b98e80297269d74b
dragonmux <git@dragonmux.network>
2022-12-10 06:48:18 +0000

    bootstrap/core/utf8: Fixed some over-long lines

--- src/bootstrap/core/utf8/char.hxx
+++ src/bootstrap/core/utf8/char.hxx
@@ -114,13 +114,16 @@ namespace mangrove::core::utf8
 					if (!(byteC & 0x80) && !file.seekRel(-1))
 						return invalidCodePoint; // fast-return if the seek fails
 					if ((byteA & 0x70U) == 0x60U && (byteC & 0xc0U) == 0x80U)
-						return encode((uint32_t(byteA & 0x0fU) << 12U) | (uint32_t(byteB & 0x3fU) << 6U) | (byteC & 0x3fU), 3U);
+						return encode(
+							(uint32_t(byteA & 0x0fU) << 12U) | (uint32_t(byteB & 0x3fU) << 6U) | (byteC & 0x3fU), 3U
+						);
 					if ((byteA & 0x78U) == 0x70U && (byteC & 0xc0U) == 0x80U)
 					{
 						const auto byteD{readByte(file)};
 						if ((byteD & 0xc0) == 0x80U)
-							return  encode(
-								(uint32_t(byteA & 0x07U) << 18U) | (uint32_t(byteB & 0x3fU) << 12U) | (uint32_t(byteC & 0x3fU) << 6U) | (byteD & 0x3fU), 4U
+							return encode(
+								(uint32_t(byteA & 0x07U) << 18U) | (uint32_t(byteB & 0x3fU) << 12U) |
+								(uint32_t(byteC & 0x3fU) << 6U) | (byteD & 0x3fU), 4U
 							);
 						// If byte D was not a continuation byte, step back one
 						if (!file.seekRel(-1))
