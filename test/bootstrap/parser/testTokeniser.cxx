a2a80b1540185e1f91c6774c5e067bbaf217900a
dragonmux <git@dragonmux.network>
2022-12-11 17:58:09 +0000

    test/boostrap/parser: Started fleshing out the test for tokenising integral literals

--- test/bootstrap/parser/testTokeniser.cxx
+++ test/bootstrap/parser/testTokeniser.cxx
@@ -9,6 +9,8 @@ using std::filesystem::current_path;
 using std::filesystem::canonical;
 using substrate::fd_t;
 using substrate::console;
+using namespace mangrove::core::utf8::literals;
+using mangrove::core::utf8::StringView;
 using mangrove::parser::Tokeniser;
 using mangrove::parser::types::TokenType;
 
@@ -16,6 +18,22 @@ class testTokeniser final : public testsuite
 {
 private:
 	path casesPath{canonical(current_path() / ".." / ".." / "cases" / "tokenisation")};
+
+	void readNewline(Tokeniser &tokeniser)
+	{
+		const auto &token = tokeniser.next();
+		assertTrue(token.valid());
+		assertEqual(token.type(), TokenType::newline);
+		assertTrue(token.value().isEmpty());
+	}
+
+	void readValue(Tokeniser &tokeniser, const TokenType expectedType, const StringView &expectedValue)
+	{
+		const auto &token = tokeniser.next();
+		assertTrue(token.valid());
+		assertEqual(token.type(), expectedType);
+		assertTrue(token.value() == expectedValue);
+	}
 
 	void testBadFileConstruction()
 	{
@@ -35,10 +53,18 @@ private:
 		const auto &token{tokeniser.token()};
 		assertFalse(token.valid());
 
-		// Consume the first token from the input and ensure it's valid
-		tokeniser.next();
-		assertTrue(token.valid());
-		// assertEqual(token.type(), TokenType::intLit);
+		// Consume the first token from the input check it tokenises as "0"
+		readValue(tokeniser, TokenType::intLit, u8"0"_sv);
+		// The next token should be a newline
+		readNewline(tokeniser);
+		// Then the test for "07"
+		readValue(tokeniser, TokenType::intLit, u8"07"_sv);
+		// The next token should be a newline
+		readNewline(tokeniser);
+		// Then the test for "08"
+		readValue(tokeniser, TokenType::intLit, u8"08"_sv);
+		// The next token should be a newline
+		readNewline(tokeniser);
 	}
 
 public:
