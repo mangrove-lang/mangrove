0fcfb6ddc517b3559dc3dd038794cd4b0879596d
dragonmux <git@dragonmux.network>
2022-12-11 18:09:07 +0000

    test/boostrap/parser: Finished the test for tokenising integral literals

--- test/bootstrap/parser/testTokeniser.cxx
+++ test/bootstrap/parser/testTokeniser.cxx
@@ -7,6 +7,7 @@
 using std::filesystem::path;
 using std::filesystem::current_path;
 using std::filesystem::canonical;
+using namespace std::literals::string_view_literals;
 using substrate::fd_t;
 using substrate::console;
 using namespace mangrove::core::utf8::literals;
@@ -21,7 +22,7 @@ private:
 
 	void readNewline(Tokeniser &tokeniser)
 	{
-		const auto &token = tokeniser.next();
+		const auto &token{tokeniser.next()};
 		assertTrue(token.valid());
 		assertEqual(token.type(), TokenType::newline);
 		assertTrue(token.value().isEmpty());
@@ -29,10 +30,26 @@ private:
 
 	void readValue(Tokeniser &tokeniser, const TokenType expectedType, const StringView &expectedValue)
 	{
-		const auto &token = tokeniser.next();
+		const auto &token{tokeniser.next()};
 		assertTrue(token.valid());
 		assertEqual(token.type(), expectedType);
 		assertTrue(token.value() == expectedValue);
+	}
+
+	void readEmptyValue(Tokeniser &tokeniser, const TokenType expectedType)
+	{
+		const auto &token{tokeniser.next()};
+		assertTrue(token.valid());
+		assertEqual(token.type(), expectedType);
+		assertTrue(token.value().isEmpty());
+	}
+
+	void readEOF(Tokeniser &tokeniser)
+	{
+		const auto &token{tokeniser.next()};
+		assertTrue(token.valid());
+		assertEqual(token.type(), TokenType::eof);
+		assertTrue(token.value().isEmpty());
 	}
 
 	void testBadFileConstruction()
@@ -53,18 +70,47 @@ private:
 		const auto &token{tokeniser.token()};
 		assertFalse(token.valid());
 
-		// Consume the first token from the input check it tokenises as "0"
+		// It is assumed after each test value that a single Linux-style new line follows
+		// Consume the first token from the input and start testing tokenisation
+		console.info("Checking tokenisation of '0'"sv);
 		readValue(tokeniser, TokenType::intLit, u8"0"_sv);
-		// The next token should be a newline
 		readNewline(tokeniser);
-		// Then the test for "07"
+		console.info("Checking tokenisation of '07'"sv);
 		readValue(tokeniser, TokenType::intLit, u8"07"_sv);
-		// The next token should be a newline
 		readNewline(tokeniser);
-		// Then the test for "08"
+		console.info("Checking tokenisation of '08'"sv);
 		readValue(tokeniser, TokenType::intLit, u8"08"_sv);
-		// The next token should be a newline
 		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0b1001'"sv);
+		readValue(tokeniser, TokenType::binLit, u8"1001"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0b'"sv);
+		readEmptyValue(tokeniser, TokenType::binLit);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0c11'"sv);
+		readValue(tokeniser, TokenType::octLit, u8"11"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0c'"sv);
+		readEmptyValue(tokeniser, TokenType::octLit);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0x95'"sv);
+		readValue(tokeniser, TokenType::hexLit, u8"95"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0x'"sv);
+		readEmptyValue(tokeniser, TokenType::hexLit);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '100'"sv);
+		readValue(tokeniser, TokenType::intLit, u8"100"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '6'"sv);
+		readValue(tokeniser, TokenType::intLit, u8"6"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of '0a'"sv);
+		readValue(tokeniser, TokenType::intLit, u8"0"_sv);
+		readValue(tokeniser, TokenType::ident, u8"a"_sv);
+		readNewline(tokeniser);
+		// Finally, consume one last token and make sure it's the EOF token
+		readEOF(tokeniser);
 	}
 
 public:
