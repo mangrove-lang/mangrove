b52099b1bce082cb8a0bbda11917b42472afde14
dragonmux <git@dragonmux.network>
2022-12-16 18:17:14 +0000

    test/bootstrap/parser: Begun implementing testing tokenisation of string/character literals

--- test/bootstrap/parser/testTokeniser.cxx
+++ test/bootstrap/parser/testTokeniser.cxx
@@ -125,12 +125,50 @@ private:
 		readEOF(tokeniser);
 	}
 
+	void testStringLiterals()
+	{
+		auto tokeniser{tokeniserFor("stringLiterals.case"sv)};
+		// Check that the tokeniser start off in an invalid state
+		const auto &token{tokeniser.token()};
+		assertFalse(token.valid());
+
+		// It is assumed after each test value that a single Linux-style new line follows
+		// Consume the first token from the input and start testing tokenisation
+		console.info("Checking tokenisation of \"\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8""_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of \"The quick brown fox jumped over the lazy dog\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8"The quick brown fox jumped over the lazy dog"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of \"\ufffd\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8"\ufffd"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of \"⛏\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8"⛏"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of \"🥭💖\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8"🥭💖"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of \"🎉 unicode literals 🎊\""sv);
+		readValue(tokeniser, TokenType::stringLit, u8"🎉 unicode literals 🎊"_sv);
+		readNewline(tokeniser);
+		// console.info("Checking tokenisation of ''"sv);
+		// readValue(tokeniser, TokenType::charLit, u8""_sv);
+		// readNewline(tokeniser);
+		// console.info("Checking tokenisation of '🍑'"sv);
+		// readValue(tokeniser, TokenType::charLit, u8"🍑"_sv);
+		// readNewline(tokeniser);
+		// Finally, consume one last token and make sure it's the EOF token
+		//readEOF(tokeniser);
+	}
+
 public:
 	void registerTests() final
 	{
 		console = {stdout, stderr};
 		CRUNCHpp_TEST(testBadFileConstruction)
 		CRUNCHpp_TEST(testIntegralLiterals)
+		CRUNCHpp_TEST(testStringLiterals)
 	}
 };
 
