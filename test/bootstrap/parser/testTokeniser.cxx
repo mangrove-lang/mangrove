b5b05c0932dbf8112f5c3d3c0be0ea1ba81add68
dragonmux <git@dragonmux.network>
2022-12-22 11:31:51 +0000

    test/bootstrap/parser: Implemented a test of the keywords tokenisation

--- test/bootstrap/parser/testTokeniser.cxx
+++ test/bootstrap/parser/testTokeniser.cxx
@@ -229,6 +229,118 @@ private:
 		readEOF(tokeniser);
 	}
 
+	void testKeywords()
+	{
+		auto tokeniser{tokeniserFor("keywords.case"sv)};
+		// Check that the tokeniser start off in an invalid state
+		const auto &token{tokeniser.token()};
+		assertFalse(token.valid());
+
+		// It is assumed after each test value that a single Linux-style new line follows
+		// Consume the first token from the input and start testing tokenisation
+		console.info("Checking tokenisation of 'true'"sv);
+		readValue(tokeniser, TokenType::boolLit, u8"true"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'false'"sv);
+		readValue(tokeniser, TokenType::boolLit, u8"false"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'nullptr'"sv);
+		readValue(tokeniser, TokenType::nullptrLit, u8"nullptr"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'and'"sv);
+		readValue(tokeniser, TokenType::logicOp, u8"&"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'or'"sv);
+		readValue(tokeniser, TokenType::logicOp, u8"|"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'not'"sv);
+		readValue(tokeniser, TokenType::logicOp, u8"!"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'eeprom'"sv);
+		readValue(tokeniser, TokenType::locationSpec, u8"eeprom"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'flash'"sv);
+		readValue(tokeniser, TokenType::locationSpec, u8"flash"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'rom'"sv);
+		readValue(tokeniser, TokenType::locationSpec, u8"rom"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'const'"sv);
+		readValue(tokeniser, TokenType::storageSpec, u8"const"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'static'"sv);
+		readValue(tokeniser, TokenType::storageSpec, u8"static"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'volatile'"sv);
+		readValue(tokeniser, TokenType::storageSpec, u8"volatile"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'new'"sv);
+		readValue(tokeniser, TokenType::newStmt, u8"new"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'delete'"sv);
+		readValue(tokeniser, TokenType::deleteStmt, u8"delete"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'from'"sv);
+		readValue(tokeniser, TokenType::fromStmt, u8"from"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'import'"sv);
+		readValue(tokeniser, TokenType::importStmt, u8"import"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'as'"sv);
+		readValue(tokeniser, TokenType::asStmt, u8"as"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'return'"sv);
+		readValue(tokeniser, TokenType::returnStmt, u8"return"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'if'"sv);
+		readValue(tokeniser, TokenType::ifStmt, u8"if"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'elif'"sv);
+		readValue(tokeniser, TokenType::elifStmt, u8"elif"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'else'"sv);
+		readValue(tokeniser, TokenType::elseStmt, u8"else"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'for'"sv);
+		readValue(tokeniser, TokenType::forStmt, u8"for"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'while'"sv);
+		readValue(tokeniser, TokenType::whileStmt, u8"while"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'do'"sv);
+		readValue(tokeniser, TokenType::doStmt, u8"do"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'none'"sv);
+		readValue(tokeniser, TokenType::noneType, u8"none"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'class'"sv);
+		readValue(tokeniser, TokenType::classDef, u8"class"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'enum'"sv);
+		readValue(tokeniser, TokenType::enumDef, u8"enum"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'function'"sv);
+		readValue(tokeniser, TokenType::functionDef, u8"function"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'operator'"sv);
+		readValue(tokeniser, TokenType::operatorDef, u8"operator"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'public'"sv);
+		readValue(tokeniser, TokenType::visibility, u8"public"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'protected'"sv);
+		readValue(tokeniser, TokenType::visibility, u8"protected"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'private'"sv);
+		readValue(tokeniser, TokenType::visibility, u8"private"_sv);
+		readNewline(tokeniser);
+		console.info("Checking tokenisation of 'unsafe'"sv);
+		readValue(tokeniser, TokenType::unsafe, u8"unsafe"_sv);
+		readNewline(tokeniser);
+		// Finally, consume one last token and make sure it's the EOF token
+		readEOF(tokeniser);
+	}
+
 public:
 	void registerTests() final
 	{
@@ -237,6 +349,7 @@ public:
 		CRUNCHpp_TEST(testIntegralLiterals)
 		CRUNCHpp_TEST(testStringLiterals)
 		CRUNCHpp_TEST(testAssignments)
+		CRUNCHpp_TEST(testKeywords)
 	}
 };
 
