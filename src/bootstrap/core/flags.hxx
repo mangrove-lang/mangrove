fe1bf57e923a5cd98e4cc5d205d31b5c9a4a4245
dragonmux <git@dragonmux.network>
2023-01-16 01:32:35 +0000

    bootstrap/core: Implemented a rawer Flags enum holder to allow the ELF SectionFlag enum to work properly

--- src/bootstrap/core/flags.hxx
+++ src/bootstrap/core/flags.hxx
@@ -63,11 +63,72 @@ namespace mangrove::core
 			return {T(newValue)};
 		}
 
-		constexpr T toRaw() const noexcept { return value; }
+		[[nodiscard]] constexpr auto toRaw() const noexcept { return value; }
 
 		constexpr bool operator ==(const BitFlags &flags) const noexcept { return value == flags.value; }
 		constexpr bool operator ==(const Enum flag) const noexcept { return value == flagAsBit(flag); }
 		constexpr bool operator !=(const BitFlags &flags) const noexcept { return value != flags.value; }
+		constexpr bool operator !=(const Enum flag) const noexcept { return value != flagAsBit(flag); }
+	};
+
+	template<typename Enum> struct Flags
+	{
+	private:
+		static_assert(std::is_enum_v<Enum>, "Flags must be enumerated by an enum");
+		using EnumInt = std::underlying_type_t<Enum>;
+
+		EnumInt value{};
+
+	public:
+		constexpr Flags() noexcept = default;
+		constexpr Flags(const Flags &flags) noexcept : value{flags.value} { }
+		// move ctor omitted as it doesn't really make sense for this type
+		constexpr Flags(const EnumInt &flags) noexcept : value{flags} { }
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr Flags(const Values ...flags) noexcept : value{(EnumInt(flags) | ...)} { }
+
+		constexpr Flags &operator =(const Flags &flags) noexcept
+		{
+			if (&flags != this)
+				value = flags.value;
+			return *this;
+		}
+
+		// Move assignment omitted as it doesn't really make sense for this type.
+
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr Flags &operator =(const Values ...flags) noexcept
+		{
+			value = (EnumInt(flags) | ...);
+			return *this;
+		}
+
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr void set(const Values ...flags) noexcept { value |= (EnumInt(flags) | ...); }
+
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr void clear(const Values ...flags) noexcept { value &= EnumInt(~(EnumInt(flags) | ...)); }
+
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr bool includes(const Values ...flags) const noexcept
+		{
+			const auto bits{(EnumInt(flags) | ...)};
+			return value & bits;
+		}
+
+		template<typename... Values, typename = std::enable_if_t<(std::is_same_v<Values, Enum> && ...)>>
+			constexpr Flags without(const Values ...flags) const noexcept
+		{
+			const auto bits{(EnumInt(flags) | ...)};
+			const auto newValue{value & ~bits};
+			return {EnumInt(newValue)};
+		}
+
+		[[nodiscard]] constexpr auto toRaw() const noexcept { return value; }
+
+		constexpr bool operator ==(const Flags &flags) const noexcept { return value == flags.value; }
+		constexpr bool operator ==(const Enum flag) const noexcept { return value == flagAsBit(flag); }
+		constexpr bool operator !=(const Flags &flags) const noexcept { return value != flags.value; }
 		constexpr bool operator !=(const Enum flag) const noexcept { return value != flagAsBit(flag); }
 	};
 } // namespace mangrove::core
